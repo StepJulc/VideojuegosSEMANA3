bd8aa7335f3a2e5ae7048c9957e5cf10252bb595
StepJulc <step.1440@gmail.com>
2023-04-10 08:48:18 -0500

    VideojuegosSEMANA3

---
+++ Assets/CarpetaNinja/CódigoNinja/NinjaControler.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NinjaControler : MonoBehaviour
+{
+    public float runSpeeed = 10;
+    public float jumpSpeed = 12 ;
+    
+    Rigidbody2D rb2D;
+    public SpriteRenderer spriteRenderer;
+
+    Animator animacion;
+    public int Idle;
+    public int Correr;
+    public int Saltar;
+    public int Atacar;
+    public int Lanzar;
+    public int Deslizar;
+    public int Morir;
+
+
+    private bool bandera = false;
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        animacion = GetComponent<Animator>();
+    }
+
+
+    void FixedUpdate()
+    {
+        if (bandera == false)
+        {
+            if (Input.GetKey("d") || Input.GetKey("right"))
+            {
+                rb2D.velocity = new Vector2(runSpeeed, rb2D.velocity.y);
+                spriteRenderer.flipX = false;
+                ChangeAnimation(Correr);
+            }
+
+            else if (Input.GetKey("a") || Input.GetKey("left"))
+            {
+                rb2D.velocity = new Vector2(-runSpeeed, rb2D.velocity.y);
+                spriteRenderer.flipX = true;
+                ChangeAnimation(Correr);
+            }
+
+            else
+            {
+                rb2D.velocity = new Vector2(0, rb2D.velocity.y);
+                ChangeAnimation(Idle);
+            }
+
+            if (Input.GetKey("space") && CheckGround.isGrounded)
+            {
+                rb2D.velocity = new Vector2(rb2D.velocity.x, jumpSpeed);
+                ChangeAnimation(Saltar);
+            }
+           
+
+
+
+        }
+
+        if (Input.GetKey("z"))
+        {
+            ChangeAnimation(Atacar);
+        }
+
+        if (Input.GetKey("x"))
+        {
+            ChangeAnimation(Lanzar);
+        }
+
+        if (Input.GetKey("v"))
+        {
+            ChangeAnimation(Deslizar);
+        }
+
+        if (Input.GetKey("b"))
+        {
+            ChangeAnimation(Morir);
+            bandera = true;
+            Destroy(gameObject, 0.8f);
+        }
+    }
+    private void ChangeAnimation(int animat)
+    {
+        animacion.SetInteger("Estado",animat);
+    }
+}
